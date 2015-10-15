65f6487475c877cabfccfd9864cf3ed921f84903
Michael V <michaelv@attunix.com>
2015-10-15 10:12:47 -0700

    version strings output

--- ComputeWebJobsSDKStorageQueue1/Program.cs
+++ ComputeWebJobsSDKStorageQueue1/Program.cs
@@ -21,6 +21,8 @@ using Newtonsoft.Json;
 using System;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace ComputeWebJobsSDKStorageQueue1
 {
@@ -41,9 +43,11 @@ namespace ComputeWebJobsSDKStorageQueue1
 
     class Program
     {
+
+
         static void Main()
         {
-            Trace.Write("Hello from job 1");
+            Trace.Write("Hello from job 1 - " + ApplicationVersionHelper.BuildString);
 
             if (!VerifyConfiguration())
             {
@@ -93,6 +97,61 @@ namespace ComputeWebJobsSDKStorageQueue1
 
             queue.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(person)));
             queue2.AddMessage(new CloudQueueMessage(JsonConvert.SerializeObject(person)));
+        }
+    }
+
+    public static class ApplicationVersionHelper
+    {
+        private static string _machineNameHash;
+        public static string MachineNameHash {
+            get {
+                if (null == _machineNameHash) {
+                    _machineNameHash = Environment.MachineName;
+                }
+                return _machineNameHash;
+            }
+        }
+
+        private static string _buildString;
+        public static string BuildString {
+            get {
+                if (null == _buildString) {
+                    try {
+                        _buildString = String.Format("{0} {1}", CurrentFileVersion, RetrieveLinkerTimestamp().ToString());
+                    }
+                    catch (Exception e) {
+                        _buildString = e.Message;
+                    }
+                }
+                return _buildString;
+            }
+        }
+
+        private static Version CurrentFileVersion { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+
+        // http://stackoverflow.com/questions/1600962/displaying-the-build-date
+        private static DateTime RetrieveLinkerTimestamp() {
+            string filePath = Assembly.GetCallingAssembly().Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+            byte[] b = new byte[2048];
+            Stream s = null;
+            try {
+                s = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                s.Read(b, 0, 2048);
+            }
+            finally {
+                if (s != null) {
+                    s.Close();
+                }
+            }
+
+            int i = BitConverter.ToInt32(b, c_PeHeaderOffset);
+            int secondsSince1970 = BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+            dt = dt.AddSeconds(secondsSince1970);
+            dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+            return dt;
         }
     }
 }
--- SecondJob/Program.cs
+++ SecondJob/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -14,10 +16,76 @@ namespace SecondJob
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
-            Console.WriteLine("job #2!");
+            Console.WriteLine("job #2! - " + ApplicationVersionHelper.BuildString);
             var host = new JobHost();
             // The following code ensures that the WebJob will be running continuously
             //host.RunAndBlock();
+        }
+    }
+    public static class ApplicationVersionHelper
+    {
+        private static string _machineNameHash;
+        public static string MachineNameHash
+        {
+            get
+            {
+                if (null == _machineNameHash)
+                {
+                    _machineNameHash = Environment.MachineName;
+                }
+                return _machineNameHash;
+            }
+        }
+
+        private static string _buildString;
+        public static string BuildString
+        {
+            get
+            {
+                if (null == _buildString)
+                {
+                    try
+                    {
+                        _buildString = String.Format("{0} {1}", CurrentFileVersion, RetrieveLinkerTimestamp().ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        _buildString = e.Message;
+                    }
+                }
+                return _buildString;
+            }
+        }
+
+        private static Version CurrentFileVersion { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+
+        // http://stackoverflow.com/questions/1600962/displaying-the-build-date
+        private static DateTime RetrieveLinkerTimestamp()
+        {
+            string filePath = Assembly.GetCallingAssembly().Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+            byte[] b = new byte[2048];
+            Stream s = null;
+            try
+            {
+                s = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                s.Read(b, 0, 2048);
+            }
+            finally
+            {
+                if (s != null)
+                {
+                    s.Close();
+                }
+            }
+
+            int i = BitConverter.ToInt32(b, c_PeHeaderOffset);
+            int secondsSince1970 = BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+            dt = dt.AddSeconds(secondsSince1970);
+            dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+            return dt;
         }
     }
 }
